1bb3d48c817fedb28181d977c9e1a10ae607d3f4
Yohans Nascimento <yohans.dev@gmail.com>
2023-06-27 23:36:19 -0300

    refactoring sql inner join command

--- src/main/java/br/com/frota/DAO/BoletoEnergiaDAO.java
+++ src/main/java/br/com/frota/DAO/BoletoEnergiaDAO.java
@@ -8,14 +8,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BoletoEnergiaDAO extends ConexaoDB{
-    private static final String JOIN_CLIENTE_DATA = "SELECT C.id, P.nome, P.cpf, P.cnpj, P.tipo_pessoa_id, C.num_cliente, C.num_documento, R.descricao, M.descricao, P.id FROM rota AS R\n" +
-            "INNER JOIN medidor M ON (M.rota_id = R.id)\n" +
-            "INNER JOIN poste AS PO ON (M.poste_id = PO.id)\n" +
-            "INNER JOIN contrato AS CO ON (CO.medidor_id = M.id)\n" +
-            "INNER JOIN cliente C ON (CO.cliente_id = C.id)\n" +
-            "INNER JOIN pessoa P ON (C.pessoa_id = P.ID)\n" +
-            "INNER JOIN tipo_pessoa AS TP ON (P.tipo_pessoa_id = TP.id)\n" +
-            "WHERE C.id = ?;";
+    private static final String JOIN_CLIENTE_DATA = "SELECT pessoa.id, pessoa.nome, pessoa.cpf, pessoa.cnpj, pessoa.tipo_pessoa_id, cliente.num_cliente, cliente.num_documento, rota.descricao, medidor.descricao\n" +
+            "FROM rota\n" +
+            "INNER JOIN medidor ON (medidor.rota_id = rota.id)\n" +
+            "INNER JOIN poste ON (medidor.poste_id = poste.id)\n" +
+            "INNER JOIN contrato ON (contrato.medidor_id = medidor.id)\n" +
+            "INNER JOIN cliente ON (contrato.cliente_id = cliente.id)\n" +
+            "INNER JOIN pessoa ON (cliente.pessoa_id = pessoa.ID)\n" +
+            "INNER JOIN tipo_pessoa ON (pessoa.tipo_pessoa_id = tipo_pessoa.id)\n" +
+            "WHERE cliente.id = ?;";
 
     public BoletoEnergia selectClienteDataById(int id) {
         BoletoEnergia entidade = null;
--- src/resources/joins.sql
+++ src/resources/joins.sql
@@ -1,8 +1,9 @@
-SELECT P.nome, P.cpf, P.cnpj, P.tipo_pessoa_id, C.num_cliente, C.num_documento, R.descricao, M.descricao, P.id FROM rota AS R
-INNER JOIN medidor M ON (M.rota_id = R.id)
-INNER JOIN poste AS PO ON (M.poste_id = PO.id)
-INNER JOIN contrato AS CO ON (CO.medidor_id = M.id)
-INNER JOIN cliente C ON (CO.cliente_id = C.id)
-INNER JOIN pessoa P ON (C.pessoa_id = P.ID)
-INNER JOIN tipo_pessoa AS TP ON (P.tipo_pessoa_id = TP.id)
-WHERE C.id = ?;
+SELECT pessoa.id, pessoa.nome, pessoa.cpf, pessoa.cnpj, pessoa.tipo_pessoa_id, cliente.num_cliente, cliente.num_documento, rota.descricao, medidor.descricao
+FROM rota
+INNER JOIN medidor ON (medidor.rota_id = rota.id)
+INNER JOIN poste ON (medidor.poste_id = poste.id)
+INNER JOIN contrato ON (contrato.medidor_id = medidor.id)
+INNER JOIN cliente ON (contrato.cliente_id = cliente.id)
+INNER JOIN pessoa ON (cliente.pessoa_id = pessoa.id)
+INNER JOIN tipo_pessoa ON (pessoa.tipo_pessoa_id = tipo_pessoa.id)
+WHERE cliente.id = ?;
